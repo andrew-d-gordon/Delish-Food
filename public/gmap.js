633babe9cb5ef5fc457d1bbcba522d81f540dd83
andrew-d-gordon <47040038+andrew-d-gordon@users.noreply.github.com>
2021-01-08 16:29:52 -0800

    Update gmaps.js to hide API key

--- public/gmap.js
+++ public/gmap.js
@@ -1,6 +1,6 @@
 // Create the script tag, set the appropriate attributes
 var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDHJ1oiP4a4X4_X9wbc13kTg9LjIs0cBfg&libraries=places&callback=initMap';
+script.src = ''; // Insert personal GMAPS API Key between '', callback => initMap()
 script.defer = true;
 
 // Append the 'script' element to 'head'
